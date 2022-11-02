e1b34720205406003150156b56ac4e87659a62ee
Damian <ddanner97@gmail.com>
2022-11-02 15:27:21 -0500

    add edit button on movie

--- src/components/ListMovieCard.js
+++ src/components/ListMovieCard.js
@@ -4,8 +4,9 @@ function ListMovieCard(props) {
   return (
     <div className='list-card-container'>
         <img className='max-h-32 ' src={`https://image.tmdb.org/t/p/original/${props.poster}`}></img>
-        <div className="rating-container">
-            <h3>Rating {props.rating}</h3>
+        <div className="rating-container flex">
+            <h3 className=''>{props.rating} / 10</h3>
+            <button onClick={() => alert('edit feature will be added soon')}><i className="fa-solid fa-pen-to-square"></i></button>
         </div>
     </div>
   )
